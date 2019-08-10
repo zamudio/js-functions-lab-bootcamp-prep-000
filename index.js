14dfecc346c9b44409418ac7e91c728168de31c3
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 00:28:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function happyHolidays {
   return "Happy Holidays!"
 }
 
-function happyHolidaysTo
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown()
